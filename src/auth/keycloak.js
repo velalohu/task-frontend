7d83169f54a5b6ba089a7ecdc9184d61ac5f4150
velalohu <80323798+velalohu@users.noreply.github.com>
2026-03-05 21:33:14 +0100

    Change Keycloak URL to production server

--- src/auth/keycloak.js
+++ src/auth/keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
 
-    url: "http://localhost:8081",
+    url: "https://auth.tasksrc.es",
     realm: "task",
     clientId: "task-frontend"
 });
